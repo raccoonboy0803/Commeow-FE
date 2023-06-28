06f7eccc017708b3693583dac30e303f76763b89
raccoonboy <raccoonboy0803@gmail.com>
2023-06-29 02:36:36 +0900

    Fix: 방송 영상 루트 수정

--- src/components/Detail/BroadDetailVideo.tsx
+++ src/components/Detail/BroadDetailVideo.tsx
@@ -9,12 +9,12 @@ const BroadDetailVideo = ({ streamer }: IBroadDetail) => {
   const edcodedString = encodeURIComponent(streamer);
 
   // const hlsUrl = `http://3.34.163.123:8080/streams/${streamer}/master.m3u8`;
-  // const hlsUrl = `http://3.34.163.123:8080/streams/${edcodedString}/master.m3u8`;
+  const hlsUrl = `http://3.34.163.123:8080/streams/${edcodedString}/master.m3u8`;
   // // const hlsUrl =
   //   'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
 
-  const hlsUrl =
-    'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
+  // const hlsUrl =
+  //   'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
 
   const handlePlayerReady = (player: ReactPlayerProps) => {
     if (player?.getInternalPlayer) {
